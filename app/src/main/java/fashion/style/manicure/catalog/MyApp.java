dd757c06d8ddf09ff648007b349f0d9a44189204
anz4sk1 <42411649+anz4sk1@users.noreply.github.com>
2018-08-18 19:27:36 +0600

    Верстка + активитти + метрика

---
+++ app/src/main/java/fashion/style/manicure/catalog/MyApp.java
@@ -0,0 +1,21 @@
+package fashion.style.manicure.catalog;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.yandex.metrica.YandexMetrica;
+import com.yandex.metrica.YandexMetricaConfig;
+
+public class MyApp extends Application {
+String metrika;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        metrika = getString(R.string.yandexmetrika);
+        YandexMetricaConfig.Builder configBuilder = YandexMetricaConfig.newConfigBuilder(metrika);
+        YandexMetrica.activate(getApplicationContext(), configBuilder.build());
+
+        YandexMetrica.enableActivityAutoTracking(this);
+
+    }
+}
