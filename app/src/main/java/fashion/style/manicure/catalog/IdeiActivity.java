f182445798b18c24f0cb59b4ee64a526214cf270
aleksejserduk <pitt.asv@yandex.ru>
2018-08-18 19:22:15 +0600

    Правильный цикл заполнения элементов 2

--- app/src/main/java/fashion/style/manicure/catalog/IdeiActivity.java
+++ app/src/main/java/fashion/style/manicure/catalog/IdeiActivity.java
@@ -14,7 +14,7 @@ import org.json.JSONObject;
 
 public class IdeiActivity extends AppCompatActivity {
 Context context = this;
-    String[][] array = new String[11][4];
+    String[][] array = new String[50][3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +26,11 @@ Context context = this;
         try {
             JSONObject obj = new JSONObject(answer);
             String ideas = obj.getString("ideas");
-            Log.e("offemainmenu", ideas);
-            for (int x = 1; x < 11; x++) {
+
+            JSONObject offersObj = new JSONObject(ideas);
+
+            for (int x = 1; x < offersObj.length()+1; x++) {
                 String ID = "id" + x;
-                JSONObject offersObj = new JSONObject(ideas);
                 String id = offersObj.getString(ID);
                 JSONObject objID = new JSONObject(id);
                 array[x][0] = objID.getString("desc");
@@ -40,22 +41,17 @@ Context context = this;
             Log.e("My App", "Could not parse malformed JSON: \"" + answer + "\"");
         }
 
-        for(int i = 1; i<11;i++){
-
+        for(int i = 1; i<7;i++){
             String ID = "imageView" + i;
             int resID = getResources().getIdentifier(ID, "id", getPackageName());
             ImageView IV = (ImageView) findViewById(resID);
             Picasso.with(context).load(array[i][1]).into(IV);
-
         }
 
     }
 
     public void nextPage(View v){
-//        final Animation animAlpha = AnimationUtils.loadAnimation(this, R.anim.alpha);
-//        v.startAnimation(animAlpha);
         String ID = v.getContentDescription().toString();
-        Log.e("dadada", ID);
         int i = Integer.parseInt(ID);
         Intent infoIntent = new Intent(IdeiActivity.this, OfferActivity.class);
         infoIntent.putExtra("desc", array[i][0]);
@@ -63,8 +59,5 @@ Context context = this;
         infoIntent.putExtra("name", array[i][2]);
 
         startActivity(infoIntent);
-    }
-    public void onClick(View view) {
-        finish();
     }
 }
